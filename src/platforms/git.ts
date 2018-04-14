68fc8217a569bfb21ebbc731262454f7e121d04b
PKief <philipp.kief@gmx.de>
2018-04-15 00:12:34 +0200

    Fixed bug with git config sections

--- src/platforms/git.ts
+++ src/platforms/git.ts
@@ -1,21 +1,29 @@
 import { execSync } from 'child_process';
+import * as path from 'path';
+import * as properties from 'properties';
+import { checkIfFileExists, getHomeDirectory } from '../helpers';
 import { UserInformation } from '../models';
 
 export const configureGitProxy = (enableProxy: boolean, u: UserInformation) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         if (enableProxy) {
             execSync(`git config --global http.proxy http://${u.username}:${u.password}@${u.host}:${u.port}`);
             execSync(`git config --global https.proxy http://${u.username}:${u.password}@${u.host}:${u.port}`);
             resolve();
         } else {
-            try {
-                execSync(`git config --global --remove-section http`);
-                execSync(`git config --global --remove-section https`);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                resolve();
+            const options = {
+                path: true,
+                sections: true,
+            };
+            const filePath = path.join(getHomeDirectory(), '.gitconfig');
+            const configfileExists = await checkIfFileExists(filePath);
+            if (configfileExists === true) {
+                properties.parse(filePath, options, (error, obj) => {
+                    if ('http' in obj) execSync(`git config --global --remove-section http`);
+                    if ('https' in obj) execSync(`git config --global --remove-section https`);
+                });
             }
+            resolve();
         }
     });
 };
